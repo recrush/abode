98a3b938a8d58d917c9d390af4deaaaa39d46d5b
Chojan Shang <psiace@outlook.com>
2023-02-13 18:17:13 +0800

    chore(license): add derivation notes
    Signed-off-by: Chojan Shang <psiace@outlook.com>

--- src/bin/catadogs.rs
+++ src/bin/catadogs.rs
@@ -1,4 +1,6 @@
-// Copyright 2022 Datafuse Labs.
+// Copyright 2023 Recrush Labs.
+//
+// Derived from opendal/oay. Copyright 2022 Datafuse Labs.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
